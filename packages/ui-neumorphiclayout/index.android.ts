892b353a89335b92ada7073de094010fe2c5e79f
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2024-11-20 16:28:47 +0200

    chore(ui-neumorphiclayout): Improved types

--- packages/ui-neumorphiclayout/index.android.ts
+++ packages/ui-neumorphiclayout/index.android.ts
@@ -1,57 +1,8 @@
-import { LayoutBase, Screen } from '@nativescript/core';
+import { LayoutBase, Screen, ViewBase } from '@nativescript/core';
 import * as commons from './common';
+import { NeumorphismState } from '.';
 
 export * from './common';
-
-function getNeumorphicDrawable(view: LayoutBase): NeumorphicDrawable {
-	const background = view.nativeViewProtected.getBackground();
-	if (background == null || background.getAugmentedCanvas == null) {
-		return null;
-	}
-	return background;
-}
-
-function refresh(value) {
-	const drawable = getNeumorphicDrawable(this);
-	if (drawable != null) {
-		drawable.invalidateSelf();
-	}
-}
-
-function toggleViewClipping(view, clipChildren: boolean): void {
-	const nativeView = view?.nativeViewProtected;
-
-	// Removing clipping from native views that extend org.nativescript.widgets.LayoutBase (this includes ContentView)
-	if (nativeView != null && nativeView instanceof org.nativescript.widgets.LayoutBase) {
-		nativeView.setClipChildren(clipChildren);
-		nativeView.setClipToPadding(clipChildren);
-	}
-}
-
-LayoutBase.prototype[commons.cornerRadiusProperty.setNative] = refresh;
-LayoutBase.prototype[commons.lightShadowColorProperty.setNative] = refresh;
-LayoutBase.prototype[commons.darkShadowColorProperty.setNative] = refresh;
-LayoutBase.prototype[commons.fillColorProperty.setNative] = refresh;
-LayoutBase.prototype[commons.shadowDistanceProperty.setNative] = refresh;
-LayoutBase.prototype[commons.shadowRadiusProperty.setNative] = refresh;
-
-LayoutBase.prototype[commons.neumorphismProperty.setNative] = function (value) {
-	const drawable = getNeumorphicDrawable(this);
-	if (value) {
-		if (drawable != null) {
-			drawable.invalidateSelf();
-		} else {
-			toggleViewClipping(this.parent, false);
-			const canvas = new commons.NeumorphicCanvas(new WeakRef(this));
-			this.nativeViewProtected.setBackground(new NeumorphicDrawable(canvas));
-		}
-	} else {
-		if (drawable != null) {
-			toggleViewClipping(this.parent, true);
-			this.nativeViewProtected.setBackground(null);
-		}
-	}
-};
 
 @NativeClass()
 class NeumorphicDrawable extends android.graphics.drawable.Drawable {
@@ -83,3 +34,58 @@ class NeumorphicDrawable extends android.graphics.drawable.Drawable {
 		return android.graphics.PixelFormat.TRANSLUCENT;
 	}
 }
+
+function _getNeumorphicDrawable(view: LayoutBase): NeumorphicDrawable {
+	const background = (view.nativeViewProtected as android.view.View).getBackground();
+
+	if (background == null || !(background instanceof NeumorphicDrawable)) {
+		return null;
+	}
+
+	return background;
+}
+
+function _refresh(_value) {
+	const drawable = _getNeumorphicDrawable(this);
+	if (drawable != null) {
+		drawable.invalidateSelf();
+	}
+}
+
+function _toggleViewClipping(view: ViewBase, clipChildren: boolean): void {
+	const nativeView = view?.nativeViewProtected as android.view.View;
+
+	// Removing clipping from native views that extend org.nativescript.widgets.LayoutBase (this includes ContentView)
+	if (nativeView != null && nativeView instanceof org.nativescript.widgets.LayoutBase) {
+		nativeView.setClipChildren(clipChildren);
+		nativeView.setClipToPadding(clipChildren);
+	}
+}
+
+function _updateNeumorphismState(this: LayoutBase, value: NeumorphismState): void {
+	const drawable = _getNeumorphicDrawable(this);
+	const nativeView = this.nativeViewProtected as android.view.View;
+
+	if (value) {
+		if (drawable != null) {
+			drawable.invalidateSelf();
+		} else {
+			_toggleViewClipping(this.parent, false);
+			const canvas = new commons.NeumorphicCanvas(new WeakRef(this));
+			nativeView.setBackground(new NeumorphicDrawable(canvas));
+		}
+	} else {
+		if (drawable != null) {
+			_toggleViewClipping(this.parent, true);
+			nativeView.setBackground(null);
+		}
+	}
+}
+
+LayoutBase.prototype[commons.cornerRadiusProperty.setNative] = _refresh;
+LayoutBase.prototype[commons.lightShadowColorProperty.setNative] = _refresh;
+LayoutBase.prototype[commons.darkShadowColorProperty.setNative] = _refresh;
+LayoutBase.prototype[commons.fillColorProperty.setNative] = _refresh;
+LayoutBase.prototype[commons.shadowDistanceProperty.setNative] = _refresh;
+LayoutBase.prototype[commons.shadowRadiusProperty.setNative] = _refresh;
+LayoutBase.prototype[commons.neumorphismProperty.setNative] = _updateNeumorphismState;
--- packages/ui-neumorphiclayout/index.ios.ts
+++ packages/ui-neumorphiclayout/index.ios.ts
@@ -1,116 +1,10 @@
-import { LayoutBase, Screen } from '@nativescript/core';
+import { EventData, LayoutBase, Screen } from '@nativescript/core';
 import * as commons from './common';
+import { NeumorphicLayout, NeumorphismState } from '.';
 
 export * from './common';
 
-const layerNames = ['bottomDrawable', 'topDrawable'];
-
-function getDrawableLayers(view: LayoutBase): Array<any> {
-	const drawableLayers: Array<any> = [];
-	const sublayers = view.nativeViewProtected?.layer?.sublayers;
-
-	if (sublayers != null) {
-		const count = sublayers.count;
-		for (let i = 0; i < count && drawableLayers.length < layerNames.length; i++) {
-			const layer = sublayers.objectAtIndex(i);
-			layerNames.includes(layer.name) && drawableLayers.push(layer);
-		}
-	}
-	return drawableLayers;
-}
-
-function refresh(value) {
-	const drawableLayers = getDrawableLayers(this);
-	if (drawableLayers.length) {
-		updateSublayerShadows(this, drawableLayers);
-	}
-}
-
-function updateSublayerBounds(args) {
-	const view = args.object;
-
-	const drawableLayers = getDrawableLayers(view);
-	for (let layer of drawableLayers) {
-		layer.frame = view.nativeViewProtected.bounds;
-		layer.setNeedsDisplay();
-	}
-}
-
-function updateSublayerShadows(view, sublayers) {
-	const [bottomLayer, topLayer] = sublayers;
-	if (bottomLayer == null || topLayer == null) {
-		throw new Error('No shadow sublayers found!');
-	}
-
-	const state = view.neumorphism;
-	if (!state) {
-		throw new Error('No neumorphism state found!');
-	}
-
-	const actualSize = view.getActualSize();
-	const width = actualSize.width;
-	const height = actualSize.height;
-	const cornerRadius = Math.min(Math.min(width, height) / 2, view.cornerRadius);
-
-	bottomLayer.cornerRadius = cornerRadius;
-	bottomLayer.backgroundColor = view.fillColor.ios.CGColor;
-	bottomLayer.shadowColor = view.lightShadowColor.ios.CGColor;
-	bottomLayer.shadowOffset = CGSizeMake(-view.shadowDistance, -view.shadowDistance);
-	bottomLayer.shadowRadius = view.shadowRadius || view.shadowDistance * 2;
-	bottomLayer.shadowOpacity = state == commons.STATE_PRESSED ? 0 : 1;
-
-	topLayer.cornerRadius = cornerRadius;
-	topLayer.backgroundColor = view.fillColor.ios.CGColor;
-	topLayer.contentsScale = Screen.mainScreen.scale;
-	topLayer.allowsEdgeAntialiasing = true;
-	topLayer.shadowColor = view.darkShadowColor.ios.CGColor;
-	topLayer.shadowOffset = CGSizeMake(view.shadowDistance, view.shadowDistance);
-	topLayer.shadowRadius = view.shadowRadius || view.shadowDistance * 2;
-	topLayer.shadowOpacity = state == commons.STATE_PRESSED ? 0 : 1;
-
-	topLayer.setNeedsDisplay();
-}
-
-LayoutBase.prototype[commons.cornerRadiusProperty.setNative] = refresh;
-LayoutBase.prototype[commons.lightShadowColorProperty.setNative] = refresh;
-LayoutBase.prototype[commons.darkShadowColorProperty.setNative] = refresh;
-LayoutBase.prototype[commons.fillColorProperty.setNative] = refresh;
-LayoutBase.prototype[commons.shadowDistanceProperty.setNative] = refresh;
-LayoutBase.prototype[commons.shadowRadiusProperty.setNative] = refresh;
-
-LayoutBase.prototype[commons.neumorphismProperty.setNative] = function (value) {
-	const drawableLayers = getDrawableLayers(this);
-	if (value) {
-		if (drawableLayers.length) {
-			updateSublayerShadows(this, drawableLayers);
-		} else {
-			const canvas = new commons.NeumorphicCanvas(new WeakRef(this));
-
-			const bottomLayer = CALayer.layer();
-			bottomLayer.name = 'bottomDrawable';
-			bottomLayer.zPosition = -2;
-
-			const topLayer = CANeumorphicLayer.initWithCanvas(canvas);
-			topLayer.name = 'topDrawable';
-			topLayer.zPosition = -1;
-
-			this.nativeViewProtected.layer.insertSublayerAtIndex(bottomLayer, 0);
-			this.nativeViewProtected.layer.insertSublayerAbove(topLayer, bottomLayer);
-
-			updateSublayerShadows(this, [bottomLayer, topLayer]);
-			updateSublayerBounds({ object: this });
-
-			this.on(LayoutBase.layoutChangedEvent, updateSublayerBounds);
-		}
-	} else {
-		if (drawableLayers.length) {
-			for (let layer of drawableLayers) {
-				layer.removeFromSuperlayer();
-			}
-		}
-		this.off(LayoutBase.layoutChangedEvent, updateSublayerBounds);
-	}
-};
+const layerNames = ['backgroundDrawable', 'foregroundDrawable'];
 
 @NativeClass
 class CANeumorphicLayer extends CALayer implements CALayerDelegate {
@@ -136,3 +30,117 @@ class CANeumorphicLayer extends CALayer implements CALayerDelegate {
 		return this.mAugmentedCanvas;
 	}
 }
+
+function _getDrawableLayers(view: NeumorphicLayout): CALayer[] {
+	const drawableLayers: CALayer[] = [];
+	const sublayers = (view.nativeViewProtected as UIView)?.layer?.sublayers;
+
+	if (sublayers != null) {
+		const count = sublayers.count;
+
+		for (let i = 0, length = sublayers.count; i < length && drawableLayers.length < layerNames.length; i++) {
+			const layer: CALayer = sublayers.objectAtIndex(i);
+
+			if (layerNames.includes(layer.name)) {
+				drawableLayers.push(layer);
+			}
+		}
+	}
+	return drawableLayers;
+}
+
+function refresh(_value) {
+	const drawableLayers = _getDrawableLayers(this);
+
+	if (drawableLayers.length) {
+		_updateSublayerShadows(this, drawableLayers);
+	}
+}
+
+function _updateSublayerBounds(view: NeumorphicLayout) {
+	const drawableLayers = _getDrawableLayers(view);
+
+	for (const layer of drawableLayers) {
+		layer.frame = view.nativeViewProtected.bounds;
+		layer.setNeedsDisplay();
+	}
+}
+
+function _updateSublayerShadows(view: NeumorphicLayout, sublayers: CALayer[]) {
+	const [bgLayer, fgLayer] = sublayers;
+	if (bgLayer == null || fgLayer == null) {
+		throw new Error('No shadow sublayers found!');
+	}
+
+	const state = view.neumorphism;
+	if (!state) {
+		throw new Error('No neumorphism state found!');
+	}
+
+	const { width, height } = view.getActualSize();
+	const cornerRadius = Math.min(Math.min(width, height) / 2, view.cornerRadius);
+
+	bgLayer.cornerRadius = cornerRadius;
+	bgLayer.backgroundColor = view.fillColor.ios.CGColor;
+	bgLayer.shadowColor = view.lightShadowColor.ios.CGColor;
+	bgLayer.shadowOffset = CGSizeMake(-view.shadowDistance, -view.shadowDistance);
+	bgLayer.shadowRadius = view.shadowRadius || view.shadowDistance * 2;
+	bgLayer.shadowOpacity = state == commons.STATE_PRESSED ? 0 : 1;
+
+	fgLayer.cornerRadius = cornerRadius;
+	fgLayer.backgroundColor = view.fillColor.ios.CGColor;
+	fgLayer.contentsScale = Screen.mainScreen.scale;
+	fgLayer.allowsEdgeAntialiasing = true;
+	fgLayer.shadowColor = view.darkShadowColor.ios.CGColor;
+	fgLayer.shadowOffset = CGSizeMake(view.shadowDistance, view.shadowDistance);
+	fgLayer.shadowRadius = view.shadowRadius || view.shadowDistance * 2;
+	fgLayer.shadowOpacity = state == commons.STATE_PRESSED ? 0 : 1;
+
+	fgLayer.setNeedsDisplay();
+}
+
+function _updateNeumorphismState(value: NeumorphismState): void {
+	const drawableLayers = _getDrawableLayers(this);
+	const layoutChangeListener = (args: EventData) => _updateSublayerBounds(args.object as NeumorphicLayout);
+
+	if (value) {
+		if (drawableLayers.length) {
+			_updateSublayerShadows(this, drawableLayers);
+		} else {
+			const nativeView = this.nativeViewProtected as UIView;
+			const canvas = new commons.NeumorphicCanvas(new WeakRef(this));
+
+			const bgLayer = CALayer.layer();
+			bgLayer.name = 'backgroundDrawable';
+			bgLayer.zPosition = -2;
+
+			const fgLayer = CANeumorphicLayer.initWithCanvas(canvas);
+			fgLayer.name = 'foregroundDrawable';
+			fgLayer.zPosition = -1;
+
+			nativeView.layer.insertSublayerAtIndex(bgLayer, 0);
+			nativeView.layer.insertSublayerAbove(fgLayer, bgLayer);
+
+			_updateSublayerShadows(this, [bgLayer, fgLayer]);
+			_updateSublayerBounds(this);
+
+			this.on(LayoutBase.layoutChangedEvent, layoutChangeListener);
+		}
+	} else {
+		if (drawableLayers.length) {
+			for (const layer of drawableLayers) {
+				layer.removeFromSuperlayer();
+			}
+		}
+
+		this.off(LayoutBase.layoutChangedEvent, layoutChangeListener);
+	}
+}
+
+LayoutBase.prototype[commons.cornerRadiusProperty.setNative] = refresh;
+LayoutBase.prototype[commons.lightShadowColorProperty.setNative] = refresh;
+LayoutBase.prototype[commons.darkShadowColorProperty.setNative] = refresh;
+LayoutBase.prototype[commons.fillColorProperty.setNative] = refresh;
+LayoutBase.prototype[commons.shadowDistanceProperty.setNative] = refresh;
+LayoutBase.prototype[commons.shadowRadiusProperty.setNative] = refresh;
+LayoutBase.prototype[commons.neumorphismProperty.setNative] = _updateNeumorphismState;
