8d522127304a780e80c22ae42ca5cafa8a5a5da5
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2026-02-24 16:00:18 +0100

    feat: line layer

---
+++ packages/ui-maplibre/lib/layers/LineLayer/common.ts
@@ -0,0 +1,16 @@
+import { LineLayer as ILineLayer, LineLayerProperties } from '.';
+import { AbstractVectorLayer } from '../AbstractVectorLayer';
+import type { ColorSpecification, PropertyValueSpecification } from '../../Expression';
+import { PaintProperty } from '../../utils/decorators';
+
+export abstract class LineLayerCommon<T> extends AbstractVectorLayer<T, LineLayerProperties> implements ILineLayer {
+	@PaintProperty('line-color')
+	public set lineColor(value: PropertyValueSpecification<ColorSpecification>) {
+		this.setPropertyValueInternal('line-color', value);
+	}
+
+	@PaintProperty('line-width')
+	public set lineWidth(value: PropertyValueSpecification<number>) {
+		this.setPropertyValueInternal('line-width', value);
+	}
+}
---
+++ packages/ui-maplibre/lib/layers/LineLayer/index.d.ts
@@ -0,0 +1,17 @@
+import { ColorSpecification, PropertyValueSpecification } from '../../Expression';
+import { BaseSource } from '../../sources';
+import { AbstractVectorLayer } from '../AbstractVectorLayer';
+import { LayerProperties } from '../BaseLayer';
+
+export type LineLayerProperties = LayerProperties & {
+	'line-color': PropertyValueSpecification<ColorSpecification>;
+	'line-width': PropertyValueSpecification<number>;
+};
+
+export declare class LineLayer extends AbstractVectorLayer<any, LineLayerProperties> {
+	constructor(id: string, source: BaseSource);
+	public get lineColor(): PropertyValueSpecification<ColorSpecification>;
+	public set lineColor(value: PropertyValueSpecification<ColorSpecification>);
+	public get lineWidth(): PropertyValueSpecification<number>;
+	public set lineWidth(value: PropertyValueSpecification<number>);
+}
