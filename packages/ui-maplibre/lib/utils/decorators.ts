f67106710159c3b308c308e8a0f4d59cf8b39447
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2026-06-09 15:41:58 +0300

    fix(ui-maplibre): Avoid duplicate property getters

--- packages/ui-maplibre/lib/utils/decorators.ts
+++ packages/ui-maplibre/lib/utils/decorators.ts
@@ -1,3 +1,4 @@
+import { Trace } from '@nativescript/core';
 import type { BaseLayer } from '../layers';
 
 function applyLayerPropertyDecorator(name: string, target: typeof BaseLayer.prototype, propertyKey: string, isPaint: boolean) {
@@ -14,21 +15,27 @@ function applyLayerPropertyDecorator(name: string, target: typeof BaseLayer.prot
 		}
 	}
 
-	const propertyDescriptor = {
-		get(this: BaseLayer) {
-			return this._getOrSetPropertyValueInternal(name, () => (typeof this[`get_native_${propertyKey}`] === 'function' ? this[`get_native_${propertyKey}`]() : null));
-		},
-		set(this: BaseLayer, value) {
-			this._setPropertyValueInternal(name, value);
-			if (typeof this[`set_native_${propertyKey}`] === 'function') {
-				this[`set_native_${propertyKey}`](value);
-			}
-		},
-	};
+	if (target[mapPropertyKey].has(name)) {
+		Trace.write(`Duplicate property '${name}' with key '${propertyKey}' for class ${target.constructor.name}`, Trace.categories.Error, Trace.messageType.warn);
+	} else {
+		const propertyDescriptor = {
+			get(this: BaseLayer) {
+				return this._getOrSetPropertyValueInternal(name, () => (typeof this[`get_native_${propertyKey}`] === 'function' ? this[`get_native_${propertyKey}`]() : null));
+			},
+			set(this: BaseLayer, value) {
+				this._setPropertyValueInternal(name, value);
+				if (typeof this[`set_native_${propertyKey}`] === 'function') {
+					this[`set_native_${propertyKey}`](value);
+				}
+			},
+		};
 
-	Object.defineProperty(target, propertyKey, propertyDescriptor);
-	Object.defineProperty(target, name, propertyDescriptor);
-	target[mapPropertyKey].add(name);
+		Object.defineProperty(target, propertyKey, propertyDescriptor);
+		if (propertyKey != name) {
+			Object.defineProperty(target, name, propertyDescriptor);
+		}
+		target[mapPropertyKey].add(name);
+	}
 }
 
 export function LayoutProperty(name: string): Function {
