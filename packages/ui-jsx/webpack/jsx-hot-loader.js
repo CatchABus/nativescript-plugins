43edd3ecc1b1264628c3c54a43f386cffa6b6a2e
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2026-07-11 14:57:27 +0300

    chore(ui-jsx): Avoid setting component id when hmr is not available

--- packages/ui-jsx/webpack/jsx-hot-loader.js
+++ packages/ui-jsx/webpack/jsx-hot-loader.js
@@ -6,17 +6,19 @@ export default function loader(content, map) {
 			/* NATIVESCRIPT-JSX-HOT-LOADER */
       import { JSXHelper } from '@nativescript-community/ui-jsx';
 
-      const componentId = __webpack_module__?.id;
+			if (__webpack_module__?.hot) {
+				const componentId = __webpack_module__?.id;
 
-      if (componentId != null && typeof __webpack_module__?.exports?.default === 'function' && __webpack_module__.exports.default._jsxComponentId !== componentId) {
-        __webpack_module__.exports.default._jsxComponentId = componentId;
-      }
+				if (componentId != null && typeof __webpack_module__.exports?.default === 'function' && __webpack_module__.exports.default._jsxComponentId !== componentId) {
+					__webpack_module__.exports.default._jsxComponentId = componentId;
+				}
 
-			if (__webpack_module__?.hot?.accept) {
-        if (componentId != null && typeof __webpack_module__.exports?.default === 'function') {
-          JSXHelper.__liveSyncNavigationEntry(componentId, __webpack_module__.exports.default);
-        }
-				__webpack_module__.hot.accept();
+				if (__webpack_module__.hot.accept) {
+					if (componentId != null && typeof __webpack_module__.exports?.default === 'function') {
+						JSXHelper.__liveSyncNavigationEntry(componentId, __webpack_module__.exports.default);
+					}
+					__webpack_module__.hot.accept();
+				}
 			}
 		`
 		: ``;
