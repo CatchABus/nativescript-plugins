53df11719b4318bd8fc059423660713181ef6fc4
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2026-07-15 17:05:07 +0300

    fix(ui-jsx): Added type declaration for scss modules

--- packages/ui-jsx/typings/shims.d.ts
+++ packages/ui-jsx/typings/shims.d.ts
@@ -3,3 +3,8 @@ declare module '*.css' {
 	const content: string | object; // The object type refers to CSS AST provided by bundlers
 	export default content;
 }
+
+declare module '*.scss' {
+	const content: string | object; // The object type refers to CSS AST provided by bundlers
+	export default content;
+}
