6e36aa55cdaa8d396377044add3913f48e6cf813
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2026-09-03 13:55:31 +0300

    feat(app-update): Added install error code enum

--- packages/app-update/index.d.ts
+++ packages/app-update/index.d.ts
@@ -2,6 +2,7 @@ export * from './lib/AppUpdateInfo';
 export * from './lib/AppUpdateManager';
 export * from './lib/AppUpdateOptions';
 export * from './lib/AppUpdateType';
+export * from './lib/InstallErrorCode';
 export * from './lib/InstallStatus';
 export * from './lib/UpdatePrecondition';
 export * from './lib/UpdateAvailability';
--- packages/app-update/lib/AppUpdateManager/index.ios.ts
+++ packages/app-update/lib/AppUpdateManager/index.ios.ts
@@ -2,17 +2,18 @@ import { Http } from '@nativescript/core';
 import { AppUpdateInfo } from '../AppUpdateInfo';
 import { AppUpdateOptions } from '../AppUpdateOptions';
 import { AppUpdateManagerCommon } from './common';
+import { InstallErrorCode } from '../InstallErrorCode';
 
 export class AppUpdateManager extends AppUpdateManagerCommon {
 	public getAppUpdateInfo(): Promise<AppUpdateInfo> {
 		return new Promise((resolve, reject) => {
 			const bundleId = NSBundle.mainBundle.bundleIdentifier;
 			Http.getJSON(`https://itunes.apple.com/lookup?bundleId=${bundleId}`).then(
-				(value: any) => {
-					let appData: object;
+				(payload: any) => {
+					let appData: any;
 
-					if (value?.resultCount > 0) {
-						appData = value.results.find((res) => res.bundleId === bundleId);
+					if (payload?.resultCount > 0) {
+						appData = payload.results.find((res) => res.bundleId === bundleId);
 					} else {
 						appData = null;
 					}
@@ -21,11 +22,11 @@ export class AppUpdateManager extends AppUpdateManagerCommon {
 						const appUpdateInfo = new AppUpdateInfo(appData);
 						resolve(appUpdateInfo);
 					} else {
-						reject(new Error('AppUdateManager Error: No app found in store with bundle ID ' + bundleId));
+						reject(new Error('AppUdateManager Error: No app found in store with bundle ID ' + bundleId + ' - Install error code: ' + InstallErrorCode.ERROR_APP_NOT_OWNED));
 					}
 				},
 				(err: Error) => {
-					reject(new Error('AppUdateManager ' + err.message));
+					reject(new Error('AppUdateManager ' + err.message + ' - Install error code: ' + InstallErrorCode.ERROR_STORE_NOT_FOUND));
 				}
 			);
 		});
